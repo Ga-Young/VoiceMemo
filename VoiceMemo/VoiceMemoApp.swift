2ebe05ca4af0a5979cd45584b09fe482b24ce5ed
gayoung <rkdudwydkv@gmail.com>
2024-11-25 15:21:56 +0900

    Initial Commit

---
+++ VoiceMemo/VoiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoApp.swift
+//  VoiceMemo
+//
+//  Created by Gayoung Kim on 11/25/24.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoiceMemoTests/VoiceMemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoTests.swift
+//  VoiceMemoTests
+//
+//  Created by Gayoung Kim on 11/25/24.
+//
+
+import Testing
+@testable import VoiceMemo
+
+struct VoiceMemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
