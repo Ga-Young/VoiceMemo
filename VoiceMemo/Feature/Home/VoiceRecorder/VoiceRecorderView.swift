74f97574754b60ab6e7e37aa6f932dfd8edc08a4
gayoung <rkdudwydkv@gmail.com>
2025-01-22 18:23:12 +0900

    Add VoiceRecorder - ViewModel

---
+++ VoiceMemo/Feature/Home/VoiceRecorder/VoiceRecorderView.swift
@@ -0,0 +1,27 @@
+//
+//  VoiceRecorderView.swift
+//  VoiceMemo
+//
+//  Created by Gayoung Kim on 1/22/25.
+//
+
+import SwiftUI
+
+struct VoiceRecorderView: View {
+    @StateObject private var voiceRecorderViewModel = VoiceRecorderViewModel()
+    
+    var body: some View {
+        ZStack {
+            // 타이틀 뷰
+            
+            // 안내 뷰
+            // 보이스 레코더 리스트 뷰
+            
+            // 녹음버튼 뷰
+        }
+    }
+}
+
+#Preview {
+    VoiceRecorderView()
+}
